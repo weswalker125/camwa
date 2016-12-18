b2b508283b75a5923fa3aa17feba3c7424e7709c
Wes Walker <weswalker125@gmail.com>
2016-12-18 10:35:33 -0500

    cli usefulness, direction of camera. option to record, single snap or move.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Camwa
+
+This is a command line script (written in NodeJS for reasons I don't recall), toying with an IP camera to verify its API for controls/capture.
+
+Built and abandoned in 2016 (except for this README).
--- camwa.js
+++ camwa.js
@@ -3,18 +3,16 @@ const fs = require('fs');
 const commandLineArgs = require('command-line-args');
 
 const optionDefinitions = [
+  { name: 'action', alias: 'a', type: String},
   { name: 'host', alias: 'h', type: String },
   { name: 'username', alias: 'u', type: String, defaultValue: 'admin' },
   { name: 'password', alias: 's', type: String, defaultValue: '' },
   { name: 'port', alias: 'p', type: Number, defaultValue: 80 },
-  { name: 'output', alias: 'o', type: String, defaultValue: '/tmp/camwa' },
+  { name: 'output', alias: 'o', type: String },
   { name: 'maxHistory', type: Number, defaultValue: 100 },
-  { name: 'interval', alias: 'i', type: Number, defaultValue: 5 }
-]
-
-const options = commandLineArgs(optionDefinitions);
-
-console.log(options);
+  { name: 'interval', alias: 'i', type: Number, defaultValue: 5 },
+  { name: 'direction', type: String },
+];
 
 function takePhoto() {
     console.log("taking snapshot");
@@ -23,21 +21,28 @@ function takePhoto() {
 }
 
 function cleanUp() {
-    var outputDir = options['output'];
-    fs.readdir(outputDir, function(err, files) {
+    fs.readdir(options['output'], function(err, files) {
+        if (err) {
+            console.log(err);
+        }
         if (files.length > options['maxHistory']) {
             console.log("cleaning up output directory...");
             // delete oldest
             files.sort();
             while (files.length > options['maxHistory'] - 1) {
                 console.log("deleting file: %s", files[0]);
-                fs.unlink(outputDir +"/"+ files[0], console.log);
+                fs.unlink(options['output'] +"/"+ files[0], console.log);
                 files.shift();
             }
         }
     });
 }
 
+// Get cli args
+const options = commandLineArgs(optionDefinitions);
+console.log(options);
+
+// Connect to camera (output connection status to console)
 foscam.setup({
     host: options['host'],
     user: options['username'],
@@ -45,4 +50,41 @@ foscam.setup({
     port: options['port']
 }, console.log);
 
-setInterval(takePhoto, options['interval']*1000);
+// Validate output directory
+if (undefined === options['output']) {
+    // store locally if no output is defined
+    options['output'] = '.';
+} else {
+    // check the dir's existence
+    if (!fs.existsSync(options['output'])){
+        // create if not there
+        fs.mkdirSync(options['output']);
+    }
+}
+
+switch (options['action']) {
+    case 'record':
+        // start periodic snapping
+        setInterval(takePhoto, options['interval']*1000);
+    break;
+    case 'move':
+        // adjust camera angle
+        foscam.control.decoder (options['direction'], function () {
+            console.log ('start sleep');
+            setTimeout(function () {
+                console.log('woke.');
+                // stop rotation
+                foscam.control.decoder ('stop ' + options['direction'], function () {
+                    takePhoto();
+                });
+            }, 1500);
+            
+        });
+    break;
+    default:
+        // take single snapshot
+        takePhoto();
+    break;
+}
+console.log('done');
+return;
